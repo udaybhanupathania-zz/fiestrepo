f2ff4d0e3b44b088aa93558ee765c2e59b630c56
udaybhanupathania@gmail.com <udaybhanupathania@gmail.com>
2020-06-22 20:35:06 +0530

    My Learning of Github

---
+++ LearnPython/firstpythoncode.py
@@ -0,0 +1,24 @@
+#firstname = 'uday'
+#lastname = 'pathania' 
+#full_name = f"{firstname}{lastname.title()}"
+#print(full_name)
+#print (f"Today is{firstname}{lastname}") 
+
+a = 10 
+b = 15 
+
+c = a + b 
+print( f"\n{a} \nand \t{b} \ttogether produce {c}" ) 
+
+d = 18_10_15_0000_111
+
+print(d)
+
+a , b,c = 1,2,3
+
+print(a,b)
+print(b); print(c)
+
+ABCD = 15
+ABCD =25
+print(ABCD)
